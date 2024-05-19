78a4a59f275ed8f42e48e9a00ffc50e350ec91b3
Anubhav <anubhav.kumar@ixambee.com>
2024-05-19 15:03:58 +0530

    Draft 1

--- src/app/page.js
+++ src/app/page.js
@@ -2,111 +2,156 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="mx-auto m-0 bg-black">
+      <div className="  flex items-center py-4 md:py-2 lg:py-12 md:justify-center bg-white">
+          <Image src="/logo.png" width={100} height={100}></Image>
+          <h1 className="text-4xl font-bold">NEAR</h1>
+        </div>
+      <div className=" w-full px-2 md:px-8 bg-gray-50 pb-24 main-bg text-white">
+        
+        <div className="flex flex-col md:flex-row py-2 md:py-24 px-2 md:px-16 ">
+          <div className="w-full md:w-1/2">
+            <p className="text-2xl font-bold py-4 md:py-8">
+              STEP INTO THE NEW REALITY
+            </p>
+            <p>
+              neAR is the visionary AR app blurring the lines between the
+              digital and physical worlds. Connect, intereact, and share
+              transcendent experiences tailored for Gen Z and millenials.
+            </p>
+            <p className="text-lg font-bold py-6 md:py-4">
+              EXCITED TO BE A PART OF NEAR
+            </p>
+            <form className="flex ">
+              <button type="button" className="mx-2">
+                YES
+              </button>
+              <button type="button" className="mx-2">
+                MAYBE
+              </button>
+            </form>
+          </div>
+          <div className="w-full md:w-1/2 flex items-center justify-center flex-col flex-row py-24 md:py-0 hidden md:block">
+            <div className="w-full md:w-1/2 flex justify-end items-end">
+              <Image
+                className=""
+                src="/modal.svg"
+                width={500}
+                height={100}
+              ></Image>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-red-200 flex flex-col md:flex-row justify-between items-center justify-center reative">
+        <video autoPlay muted loop className="w-full h-1/2  z-0">
+          <source src="/mov1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="w-full md:w-1/2 m-0 md:m-8 z-1 absolute text-white">
+          <p className="text-3xl font-bold py-4">
+            neAR: Augmented Social Connection
+          </p>
+          <p className="text-2xl">
+            Unleash your social life with neAR, the augmented reality app that
+            connects you to the world around you. With cutting-edge AR
+            technology, neAR blurs the lines between the digital and physical
+            realms, allowing you to discover, interact and share moments like
+            never before.
+          </p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="bg-red-200 flex flex-col md:flex-row justify-between items-center p-4 md:p-12 justify-center">
+        <div className="w-full md:w-1/2 m-0 md:m-6 flex justify-center">
+          <Image className="" src="/exp1.png" width={200} height={500}></Image>
+        </div>
+        <div className=" w-full md:w-1/2 m-0 md:m-8">
+          <p className="text-2xl font-bold py-4">IMMERSIVE AR CAMERA</p>
+          <p>
+            Transform your surroundings into an immersive social canvas. Point
+            your camera to unveil vibrant posts, pulsating live streams, and
+            interactive AR content vividly overlaying the real world.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </div>
+      </div>
+      <div className="bg-red-50 flex flex-col md:flex-row justify-between items-center p-4 md:p-12 justify-center ">
+        <div className="w-full md:w-1/2 m-0 md:m-6 flex justify-center">
+          <Image className="" src="/exp2.png" width={200} height={500}></Image>
+        </div>
+        <div className=" w-full md:w-1/2 m-0 md:m-8">
+          <p className="text-2xl font-bold py-4">
+            RADAR: DISCOVER THE UNDISCOVERED
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p>
+            Leave ailmless searching behind. neAR's powerful Radar instantly
+            locates like-minded individuals in your vicinity. Adjust scanning
+            distance, activate incognito mode, or openly forge new
+            connection-the world is your to explore.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        </div>
+      </div>
+      <div className="bg-red-100 flex flex-col md:flex-row justify-between items-center p-4 md:p-12 justify-center">
+        <div className="w-full md:w-1/2 m-0 md:m-6 flex justify-center">
+          <Image className="" src="/exp3.png" width={200} height={500}></Image>
+        </div>
+        <div className=" w-full md:w-1/2 m-0 md:m-8">
+          <p className="text-2xl font-bold py-4">
+            MAP: A WORLD OF POSSIBILTIES
           </p>
-        </a>
+          <p>
+            Embark on an augmented journey through an interactive fusion of
+            reality and virtual realms. Zoom to uncover a tapestry of shared
+            moments. React to posts. Unlock your city's hidden AR experiences
+            waiting to be discovered.
+          </p>
+        </div>
+      </div>
+      <div className="bg-red-50 flex flex-col md:flex-row justify-between items-center p-4 md:p-12 justify-center ">
+        <div className="w-full md:w-1/2 m-0 md:m-6 flex justify-center">
+          <Image className="" src="/exp4.png" width={200} height={500}></Image>
+        </div>
+        <div className=" w-full md:w-1/2 m-0 md:m-8">
+          <p className="text-2xl font-bold py-4">
+            CHAT: TRANSCENDING BOUNDARIES
+          </p>
+          <p>
+            with neAR, connections transcend boundaries. Accept requests from
+            those around you and let conversations flow through the sleek chat
+            interface. Share thoughts, image, videos-attachment capabilities
+            foster seamless experience sharing.
+          </p>
+        </div>
+      </div>
+      <div className="bg-red-100 flex flex-col items-center justify-center p-4 md:p-12 justify-center ">
+        <p className="text-3xl font-bold ">
+          ELEVATE YOUR SOCIAL STATUS-BE AN EARLY ADOPTER OF NEAR.
+        </p>
+        <form className="my-12 md:my-8">
+          <input
+            type="text"
+            name="email"
+            className="bg-blue-50 p-4 rounded-3xl"
+            placeholder="E-mail"
+          ></input>
+          <button type="sumbit" className="rounded-full bg-blue-50 p-4">
+            ✉️
+          </button>
+        </form>
+      </div>
+      <div>
+        <div className="bg-red-200 flex flex-col items-center justify-center p-4 md:p-12 justify-center ">
+          <p className="text-3xl font-bold break-all">
+            Say Hello at nearsocialnetwork@gmail.com
+          </p>
+          <div className="flex border border-black items-center justify-center  py-4 mt-2 md:mt-8">
+            <p className="text-xl px-8">SOCIAL</p>
+            <div className="flex flex-row">
+              <div className="px-2">F</div>
+              <div className="px-2">X</div>
+              <div className="px-2">I</div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
